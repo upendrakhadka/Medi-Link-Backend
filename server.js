f5f9109e583c6737ed97f40f342b234b0b2a260f
upendrakhadka <khadka.upendra02@gmail.com>
2025-05-17 22:08:34 +0545

    adddddddd

--- server.js
+++ server.js
@@ -16,10 +16,9 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors({
-  origin: ['https://medi-link-swart.vercel.app', 'http://localhost:5173'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
-}));
+  origin: '*'
+}))
+
 
 
 // api endpoints
